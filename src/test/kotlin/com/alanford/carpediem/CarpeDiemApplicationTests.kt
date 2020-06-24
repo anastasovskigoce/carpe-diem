e51cacba7ba9118102bb608eded7111a9179de94
Goce <ganastasovski@alarm.com>
2020-06-24 10:14:20 -0400

    used kotlin + gradle + inellij

---
+++ src/test/kotlin/com/alanford/carpediem/CarpeDiemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.alanford.carpediem
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CarpeDiemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
